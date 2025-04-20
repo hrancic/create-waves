a28d0be2f34855af9ebd4d5cec1bf1766a22fee0
Hrvoje Rancic <hrvoje.rancic@gmail.com>
2025-04-20 12:37:36 +0200

    Use class for Entity object, record for value object

--- src/main/java/com/djigabajt/creativewaves/domain/Client.java
+++ src/main/java/com/djigabajt/creativewaves/domain/Client.java
@@ -1,15 +1,51 @@
 package com.djigabajt.creativewaves.domain;
 
+import java.util.Objects;
+
 import static java.util.Objects.requireNonNull;
 
-public record Client(ClientId clientId, Name name) {
+public final class Client {
+
+    private final ClientId clientId;
+    private final Name name;
 
     private Client(Name name) {
-        this(ClientId.getNext(), name);
+        this.clientId = ClientId.getNext();
+        this.name = name;
     }
 
     public static Client create(Name name) {
         requireNonNull(name, "Client name must not be null");
         return new Client(name);
     }
+
+    public ClientId clientId() {
+        return clientId;
+    }
+
+    public Name name() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        var that = (Client) obj;
+        return Objects.equals(this.clientId, that.clientId) &&
+                Objects.equals(this.name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(clientId, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Client[" +
+                "clientId=" + clientId + ", " +
+                "name=" + name + ']';
+    }
+
 }
